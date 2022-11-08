df81fa70264aafb5e7be5ed823f9e52e63f10b43
xiaoer <wang-xe@outlook.com>
2022-11-08 19:07:27 +0800

    第一次提交

---
+++ src/main/java/com/atxiao2/git_test/GitTest.java
@@ -0,0 +1,11 @@
+package com.atxiao2.git_test;
+
+/**
+ * @author : XiaoEr
+ * @date : 2022/11/7
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
