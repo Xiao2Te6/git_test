742c5eb1ba8fba366233dc114911ec4d89fed844
xiaoer <wang-xe@outlook.com>
2022-11-08 19:35:26 +0800

    main分支第四次提交

--- src/main/java/com/atxiao2/git_test/GitTest.java
+++ src/main/java/com/atxiao2/git_test/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4"); //main
 
     }
 }
